11c7b539bcdc171867106287c5c0e0e74e0e1a13
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-07-01 15:18:55 +0800

    <feat>[AC-S2-3-W1-My-Moive-List-Express]: Set up handlebars partial templates
    1. Create index.handlebars.
    2. Set up movie list.
    3. set up partial templates render.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'handlebars')
 
 // Handle request and response here
 app.get('/', (req, res) => {
-  res.send('This is my movie list built with Express')
+  res.render('index')
 })
 
 // start and listen on the Express server
